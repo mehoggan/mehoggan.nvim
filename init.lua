64260a06899e4f4f04ef3d392123220f9cff0b72
Matthew Hoggan <matthew.hoggan@garmin.com>
2026-01-20 09:00:11 -0500

    Fixes Shift-D issue.
    Change-Id: I0f26f3737751ccd82a6b5c925bd468d1aa0272a1

--- init.lua
+++ init.lua
@@ -28,15 +28,6 @@ vim.keymap.set("n", "B", function()
   fidget.notify("This is also from fidget.notify().", vim.log.levels.WARN)
 end)
 
-vim.keymap.set("n", "C", function()
-  fidget.notify("fidget.notify() supports annotations...", nil, { annote = "MY NOTE", key = "foobar" })
-end)
-
-vim.keymap.set("n", "D", function()
-  fidget.notify(nil, vim.log.levels.ERROR, { annote = "bottom text", key = "foobar" })
-  fidget.notify("... and overwriting notifications.", vim.log.levels.WARN, { annote = "YOUR AD HERE" })
-end)
-
 -- LspConfig
 vim.lsp.enable({ "pylsp" })
 vim.lsp.enable({ "pyright" })
@@ -141,6 +132,7 @@ function CScopeAdd()
 	if file_exists(cscope_file) then
 		print("Adding " .. cscope_file .. "...")
 		vim.cmd(":Cs db add")
+    print("Done with adding cscope!")
 	end
 end
 
