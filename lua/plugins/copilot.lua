2a1ce56c3008ab79893461bd48f3b12a95da4b89
Matthew Hoggan <matthew.hoggan@garmin.com>
2026-04-06 14:22:09 -0400

    Switch to LazyVim.
    Change-Id: Iead2fbe83a0f8790994dab3f22f0f5b4d38948e5

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -3,4 +3,14 @@ return {
   {
     "github/copilot.vim",
   },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "nvim-lua/plenary.nvim", branch = "master" },
+    },
+    build = "make tiktoken",
+    opts = {
+      -- See Configuration section for options
+    },
+  },
 }
