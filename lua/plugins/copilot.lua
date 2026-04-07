ebc841240cb975e9f09294cd85257ed30437d0c4
Matthew Hoggan <matthew.hoggan@garmin.com>
2026-04-07 12:33:53 -0400

    Adds key bindings for copilot.
    Change-Id: I4dafadb19c19dce0fc66e3542e033aa967d558f8

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -12,5 +12,14 @@ return {
     opts = {
       -- See Configuration section for options
     },
+    keys = {
+      { "<leader>zc", ":CopilotChat<CR>", mode = "n", desc = "Chat with Copilot" },
+      { "<leader>ze", ":CopilotChatExplain<CR>", mode = "v", desc = "Explain Code" },
+      { "<leader>zr", ":CopilotChatReview<CR>", mode = "v", desc = "Review Code" },
+      { "<leader>zf", ":CopilotChatFix<CR>", mode = "v", desc = "Fix Code Issues" },
+      { "<leader>zo", ":CopilotChatOptimize<CR>", mode = "v", desc = "Optimize Code" },
+      { "<leader>zd", ":CopilotChatDocs<CR>", mode = "v", desc = "Generate Documentation" },
+      { "<leader>zt", ":CopilotChatTest<CR>", mode = "v", desc = "Generate Tests" },
+    },
   },
 }
