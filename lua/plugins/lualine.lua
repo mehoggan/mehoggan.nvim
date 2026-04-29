8449d5aa12f03a945e614f7b7920f2ebb6a9f657
Matthew Hoggan <matthew.hoggan@garmin.com>
2026-04-29 14:57:22 -0400

    Add pomodoro timer in.
    Change-Id: Ibbd1ad8f046a17b542b162eec4a12a79f2340453

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      -- Customize lualine here
+      -- Example: table.insert(opts.sections.lualine_x, "filetype")
+    end,
+  },
+}
