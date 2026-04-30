7e6ca95245fa683ebe52950628dc2623dccff709
Matthew Hoggan <matthew.hoggan@garmin.com>
2026-04-30 13:19:29 -0400

    Updates keymaps for multiple terminals.
    Change-Id: I6f105e95b054ba4fefc68ccb651ab1c5e8452b96

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("n", "<leader>tv", function()
-  Snacks.terminal(nil, { win = { position = "right" } })
+vim.keymap.set({ "n", "t" }, "<leader>tr", function()
+  Snacks.terminal.open(nil, { win = { position = "right" } })
 end, { desc = "Terminal (Vertical Right)" })
+vim.keymap.set({ "n", "t" }, "<leader>tb", function()
+  Snacks.terminal.open(nil, { win = { position = "bottom" } })
+end, { desc = "Terminal (Horizontal Bottom)" })
