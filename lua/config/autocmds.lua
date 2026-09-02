29c212dcec370e8b48a0bb302cd5a6492173583b
Matthew Hoggan <matthew.hoggan@garmin.com>
2026-09-02 14:16:06 -0400

    feat(lua/config/autocmds.lua): add LspRestart command to restart LSP client

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,12 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+vim.api.nvim_create_user_command("LspRestart", function(o)
+  local name = o.args ~= "" and o.args or nil
+  for _, c in ipairs(vim.lsp.get_clients({ name = name })) do
+    c:stop(true)
+  end
+  vim.defer_fn(function()
+    vim.cmd.edit()
+  end, 200)
+end, { nargs = "?" })
